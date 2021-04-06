cffbdcf4bb44241dba7005708d922df58f5c904a
Robert Schrenk <robert@schrenk.cc>
2021-04-06 10:32:26 +0200

    remove check is_siteadmin

--- classes/lib.php
+++ classes/lib.php
@@ -376,7 +376,7 @@ class lib {
             $expirationtime = 0;
         }
 
-        $sql = "SELECT edu.id, edu.discussionid, edu.opened, f.id, f.timemodified 
+        $sql = "SELECT edu.id, edu.discussionid, edu.opened, f.id, f.timemodified
                 FROM {local_edusupport_issues} edu
                 JOIN {forum_discussions} f
                     ON edu.discussionid = f.id
@@ -681,7 +681,6 @@ class lib {
     **/
     public static function supportforum_enable($forumid) {
         global $DB, $USER;
-        if (!is_siteadmin()) return false;
         $forum = $DB->get_record('forum', array('id' => $forumid));
         if (empty($forum->course)) return false;
 
@@ -730,7 +729,6 @@ class lib {
     **/
     public static function supportforum_managecaps($forumid, $trigger) {
         global $DB, $USER;
-        if (!is_siteadmin()) return false;
         $forum = $DB->get_record('forum', array('id' => $forumid));
         if (empty($forum->course)) return false;
 
