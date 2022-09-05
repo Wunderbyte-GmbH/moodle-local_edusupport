8be87377ec1f87316e9955b9e67a1828dae87f07
Thomas Winkler <thomas.winkler@wunderbyte.at>
2022-09-05 08:06:56 +0200

    upgrade old holidaymode

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2022082401;
+$plugin->version  = 2022090501;
 $plugin->requires = 2020061500;
 $plugin->component = 'local_edusupport';
 $plugin->release = '2.2.8';
